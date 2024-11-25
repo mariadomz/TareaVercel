bb0f838ae7a23a26cd57b1dd916dbd9c65ab715e
Maria Dominguez <mdominguez010101@gmail.com>
2024-11-25 10:05:28 -0300

    Initial commit

--- python_vercel_task/urls.py
+++ python_vercel_task/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+# Vista para la raíz del sitio
+def home(request):
+    return HttpResponse("¡Bienvenido a mi aplicación!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('', home),  # Aquí agregamos la vista para la URL raíz
 ]
